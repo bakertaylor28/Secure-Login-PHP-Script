18b87f31b1331e420e6369223f5d62b8ba9c16a5
bakertaylor28 <bakertaylor28@gmail.com>
2017-10-14 22:39:56 -0500

    PHP Source Code

---
+++ protected_page.php
@@ -0,0 +1,31 @@
+<?php
+include_once 'db_connect.php';
+include_once 'functions.php';
+ 
+sec_session_start();
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Secure Login: Protected Page</title>
+        <link rel="stylesheet" href="styles/main.css" />
+    </head>
+    <body>
+        <?php if (login_check($mysqli) == true) : ?>
+            <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
+            <p>
+                This is an example protected page.  To access this page, users
+                must be logged in.  At some stage, we'll also check the role of
+                the user, so pages will be able to determine the type of user
+                authorised to access the page.
+            </p>
+            <p>Return to <a href="index.php">login page</a></p>
+        <?php else : ?>
+		    <h1>Error 301: Forbidden</h1>
+            <p>
+                <span class="error">You are not authorized to access this page.</span> Please <a href="index.php">login</a>.
+            </p>
+        <?php endif; ?>
+    </body>
+</html>
---
+++ psl-config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you want to connect to.
+define("USER", "id3263427_sec_user");    // The database username. 
+define("PASSWORD", "Itudmenif1!Itudmenif1!");    // The database password. 
+define("DATABASE", "id3263427_secure_login");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", TRUE);    // FALSE- FOR DEVELOPMENT ONLY!!!!
+?>
